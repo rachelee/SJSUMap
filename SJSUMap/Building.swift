6af80506eaaf0720b0309c320946b58311b828ae
rachelee <lixiaox5@gmail.com>
2015-11-09 20:17:45 -0800

    add files

--- SJSUMap/Building.swift
+++ SJSUMap/Building.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2015 xiaoxiao li. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Meal {
+    //MARK: Properties
+    
+    var name: String
+    var address: String
+    var photo: UIImage?
+    var lat: Float
+    var lng: Float
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, address: String, lat: Float, lng: Float) {
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.address = address
+        self.lat = lat
+        self.lng = lng
+        
+    }
+}
